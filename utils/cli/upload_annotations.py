cecda9203a23ad52b8538fbe77f2744e0517310c
Alexander Ivkin <a@ivkin.dev>
2022-01-27 20:53:10 -0500

    Tried uploading video with annotations - failed

--- utils/cli/upload_annotations.py
+++ utils/cli/upload_annotations.py
@@ -22,9 +22,13 @@ credentials = sys.argv[4] if len(sys.argv) > 4 else 'admin:admin'
 labels = [{"name": "disc", "color": "#f68e83", "attributes": []}]
 
 for filename in sorted(os.listdir(dir)):
+    filepath = os.path.join(dir, filename)
     if os.path.isdir(filename):
-        filepath = os.path.join(dir, filename)
         resources = [filepath + "/" + file for file in os.listdir(filepath)]
         run(['--auth', credentials, '--server-host', 'localhost', '--server-port', '8080', 'create', '--labels', json.dumps(labels),
              '--annotation_path', annotations_path, '--annotation_format', 'COCO 1.0', '--project_id', project_id, '--image_quality', '70',
              filename, 'local', resources])
+    else:
+        run(['--auth', credentials, '--server-host', 'localhost', '--server-port', '8080', 'create', '--labels', json.dumps(labels),
+             '--annotation_path', annotations_path, '--annotation_format', 'COCO 1.0', '--project_id', project_id, '--image_quality', '70',
+             filename, 'local', filepath])
